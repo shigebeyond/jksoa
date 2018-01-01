b0012b520bcf8e4b120332f4cbd2d099f9578b47
shijianhang <3076679995@qq.com>
2018-01-01 22:21:59 +0800

    完善异步响应的超时 + get()异常

--- jksoa-client/src/main/kotlin/com/jksoa/protocol/netty/NettyConnection.kt
+++ jksoa-client/src/main/kotlin/com/jksoa/protocol/netty/NettyConnection.kt
@@ -59,8 +59,7 @@ class NettyConnection(protected val channel: Channel, url: Url) : IConnection(ur
 
         // 2.1 发送成功
         if (result && writeFuture.isSuccess()) {
-            val expireTime = System.currentTimeMillis() + timeout // 过期时间
-            val resFuture = ResponseFuture(req, expireTime) // 返回异步响应
+            val resFuture = ResponseFuture(req, timeout) // 返回异步响应
             NettyResponseHandler.putResponseFuture(req.id, resFuture) // 记录异步响应，以便响应到来时设置结果
             return resFuture
         }
--- jksoa-common/src/main/kotlin/com/jksoa/common/future/ResponseFuture.kt
+++ jksoa-common/src/main/kotlin/com/jksoa/common/future/ResponseFuture.kt
@@ -2,7 +2,11 @@ package com.jksoa.common.future
 
 import com.jkmvc.future.BasicFuture
 import com.jksoa.common.IRequest
+import com.jksoa.common.exception.RpcBusinessException
+import com.jksoa.common.exception.RpcClientException
 import org.apache.http.concurrent.FutureCallback
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
 
 /**
  * 异步响应
@@ -14,7 +18,7 @@ import org.apache.http.concurrent.FutureCallback
  * @date 2017-12-30 6:43 PM
  */
 class ResponseFuture(public val request: IRequest, /* 请求 */
-                     public val expireTime: Long, /* 过期时间，单位毫秒 */
+                     public val timeout: Long, /* 超时，单位毫秒 */
                      callback: FutureCallback<Any?>? = null /* 回调 */
 ): IResponseFuture, BasicFuture<Any>(callback) {
 
@@ -24,7 +28,19 @@ class ResponseFuture(public val request: IRequest, /* 请求 */
      * @param request
      * @param callback
      */
-    public constructor(request: IRequest, callback: FutureCallback<Any?>? = null): this(request, System.currentTimeMillis() + 100000 /* 10s后超时 */, callback){}
+    public constructor(request: IRequest, callback: FutureCallback<Any?>? = null): this(request, 100000 /* 默认超时5s */, callback){}
+
+
+    /**
+     * 创建时间
+     */
+    public val createTime: Long = System.currentTimeMillis()
+
+    /**
+     * 过期时间
+     */
+    public val expireTime: Long
+        get() = createTime + timeout
 
     /**
      * 请求标识
@@ -43,4 +59,25 @@ class ResponseFuture(public val request: IRequest, /* 请求 */
      */
     public override val exception: Exception?
         get() = ex
+
+    /**
+     * 同步获得结果，有默认超时
+     * @return
+     */
+    public override fun get(): Any? {
+        return get(timeout, TimeUnit.MILLISECONDS)
+    }
+
+    public override fun get(timeout: Long, unit: TimeUnit): Any? {
+        try{
+            return super.get(timeout, unit)
+        }catch (e: ExecutionException){
+            // 业务异常
+            if(e.cause is RpcBusinessException)
+                throw e.cause!!
+
+            // 服务端异常
+            throw RpcClientException(e.cause!!)
+        }
+    }
 }
