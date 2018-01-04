72c52a05199461bf0f9be6f2c17a1cfa82de3b9a
shijianhang <772910474.com>
2018-01-04 11:30:14 +0800

    CompletedResponseFuture需要Response，而不是IResponse(有可能是IResponseFuture)

--- jksoa-common/src/main/kotlin/com/jksoa/common/future/CompletedResponseFuture.kt
+++ jksoa-common/src/main/kotlin/com/jksoa/common/future/CompletedResponseFuture.kt
@@ -2,6 +2,7 @@ package com.jksoa.common.future
 
 import com.jkmvc.future.CompletedFuture
 import com.jksoa.common.IResponse
+import com.jksoa.common.Response
 
 /**
  * 已完成的异步响应，没有延迟
@@ -11,5 +12,5 @@ import com.jksoa.common.IResponse
  * @author shijianhang<772910474@qq.com>
  * @date 2017-12-30 6:43 PM
  */
-class CompletedResponseFuture(res: IResponse): IResponseFuture, CompletedFuture<Any?>(res.value), IResponse by res {
+class CompletedResponseFuture(res: Response): IResponseFuture, CompletedFuture<Any?>(res.value), IResponse by res {
 }
