d394a64bf894a9d8d983f33475b76eb0f68a5575
shijianhang <772910474.com>
2019-01-13 18:40:16 +0800

    添加定时测试

--- jksoa-common/src/test/kotlin/com/jksoa/tests/MyTests.kt
+++ jksoa-common/src/test/kotlin/com/jksoa/tests/MyTests.kt
@@ -1,10 +1,26 @@
 package com.jksoa.tests
 
 import com.jksoa.common.Url
+import io.netty.util.HashedWheelTimer
+import io.netty.util.Timeout
+import io.netty.util.TimerTask
 import org.junit.Test
+import java.util.concurrent.TimeUnit
+
+fun main(args: Array<String>) {
+    // 测试定时器
+    // 注意: 无法使用junit来测试, 因为 HashedWheelTimer 内部会休眠线程, 会导致junit执行终止
+    val timer = HashedWheelTimer(1, TimeUnit.SECONDS, 3 /* 内部会调用normalizeTicksPerWheel()转为2的次幂, 如3转为4 */)
+    timer.newTimeout(object : TimerTask {
+        override fun run(timeout: Timeout) {
+            println("定时处理")
+            //timer.newTimeout(this, 60, TimeUnit.SECONDS)
+        }
+    }, 7, TimeUnit.SECONDS)
+}
 
 /**
- * @ClassName: ClientTests
+ * 基本测试
  * @Description:
  * @author shijianhang<772910474@qq.com>
  * @date 2017-12-14 3:11 PM
