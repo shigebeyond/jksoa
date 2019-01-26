15a488451fea17e004bca077b26cbf404c9354cf
shijianhang <772910474.com>
2019-01-26 22:04:18 +0800

    抽取共同定时器

---
+++ jksoa-common/src/main/kotlin/com/jksoa/common/_Timer.kt
@@ -0,0 +1,11 @@
+package com.jksoa.common
+
+import io.netty.util.HashedWheelTimer
+import java.util.concurrent.TimeUnit
+
+/**
+ * 共同定时器
+ */
+public val CommonTimer by lazy{
+    HashedWheelTimer(200, TimeUnit.MILLISECONDS, 300)
+}
