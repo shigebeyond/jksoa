c63d0ebf0959ee31925be7bfc76aabdd9fb78318
shijianhang <772910474.com>
2019-03-31 16:36:44 +0800

    添加默认方法的测试

--- jksoa-client/src/test/kotlin/net/jkcode/jksoa/tests/ClientTests.kt
+++ jksoa-client/src/test/kotlin/net/jkcode/jksoa/tests/ClientTests.kt
@@ -1,14 +1,21 @@
 package net.jkcode.jksoa.tests
 
+import net.jkcode.jkmvc.common.getMethodHandle
 import net.jkcode.jkmvc.common.getRootResource
+import net.jkcode.jkmvc.common.newInstance
 import net.jkcode.jksoa.client.dispatcher.RcpRequestDispatcher
+import net.jkcode.jksoa.client.protocol.netty.NettyClient
 import net.jkcode.jksoa.client.referer.Referer
 import net.jkcode.jksoa.common.ShardingRpcRequest
 import net.jkcode.jksoa.common.Url
 import net.jkcode.jksoa.example.IExampleService
 import net.jkcode.jksoa.example.ISystemService
-import net.jkcode.jksoa.client.protocol.netty.NettyClient
 import org.junit.Test
+import java.lang.reflect.Proxy
+import java.lang.invoke.MethodHandles
+import java.lang.reflect.Method
+import kotlin.reflect.jvm.javaMethod
+
 
 /**
  * @Description:
@@ -16,6 +23,49 @@ import org.junit.Test
  * @date 2017-12-14 3:11 PM
  */
 class ClientTests {
+
+    @Test
+    fun testProxy(){
+        val p = Proxy.newProxyInstance(this.javaClass.classLoader, arrayOf(ISystemService::class.java)) { proxy, method, args ->
+            println("调用方法: ${method.name}" + args?.joinToString(",", "(", ")"))
+            println("是否默认方法: " + method.isDefault)
+        } as ISystemService
+        p.echo()
+        //p.defaultMethod()
+    }
+
+    @Test
+    fun testDefaultMethod(){
+        val proxy = Proxy.newProxyInstance(Thread.currentThread().contextClassLoader, arrayOf(ISystemService::class.java)) {
+            proxy: Any, method: Method, arguments: Array<Any> -> null
+        } as ISystemService
+        // 1 直接调用报错
+        //proxy.defaultMethod()
+
+        // 2 反射调用: java.lang.IllegalAccessException: no private access for invokespecial: interface net.jkcode.jksoa.example.ISystemService, from net.jkcode.jksoa.example.ISystemService/public
+        val method = ISystemService::defaultMethod.javaMethod!!
+        /*val result = MethodHandles.lookup()
+                .`in`(method.getDeclaringClass())
+                .unreflectSpecial(method, method.getDeclaringClass())
+                .bindTo(proxy)
+                .invokeWithArguments()
+        */
+
+        // 3 反射调用
+        /*val clazz = method.declaringClass
+        //new MethodHandles.Lookup(clazz) // 包内构造方法, 不能直接调用
+        // 反射调用包内构造方法
+        //println(clazz.javaClass)
+        val constructor = MethodHandles.Lookup::class.java.getDeclaredConstructor(clazz.javaClass)
+        constructor.isAccessible = true
+        val result = constructor.newInstance(clazz)
+                .unreflectSpecial(method, clazz)
+                .invokeWithArguments(proxy, "hello")
+        */
+
+        // 4 简化
+        val result = method.getMethodHandle().invokeWithArguments(proxy, "hello")
+    }
 
     @Test
     fun testScanClass(){
