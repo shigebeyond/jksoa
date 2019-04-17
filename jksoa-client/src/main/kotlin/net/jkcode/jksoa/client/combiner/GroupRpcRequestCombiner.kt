25d5518c565e181fbcbb0a1b1952cf8e7db4e6a0
shijianhang <772910474.com>
2019-04-17 16:15:35 +0800

    支持rpc请求合并

---
+++ jksoa-client/src/main/kotlin/net/jkcode/jksoa/client/combiner/GroupRpcRequestCombiner.kt
@@ -0,0 +1,65 @@
+package net.jkcode.jksoa.client.combiner
+
+import net.jkcode.jkmvc.combiner.GroupFutureSupplierCombiner
+import net.jkcode.jksoa.client.combiner.annotation.GroupCombine
+import net.jkcode.jksoa.client.dispatcher.IRpcRequestDispatcher
+import net.jkcode.jksoa.client.dispatcher.RcpRequestDispatcher
+import net.jkcode.jksoa.common.getServiceClass
+import net.jkcode.jksoa.common.groupCombine
+import java.lang.reflect.Method
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.reflect.KFunction2
+import kotlin.reflect.jvm.javaMethod
+
+/**
+ * 针对每个group的取值操作合并, 每个group攒够一定数量/时间的请求才执行
+ *    如请求合并/cache合并等
+ *
+ * @author shijianhang<772910474@qq.com>
+ * @date 2019-04-10 9:47 AM
+ */
+class GroupRpcRequestCombiner<RequestArgumentType /* 请求参数类型 */, ResponseType /* 响应类型 */, BatchItemType: Any /* 批量取值操作的返回列表的元素类型 */>(
+        reqArgField: String, /* 请求参数对应的响应字段名 */
+        respField: String = "", /* 要返回的响应字段名, 如果为空则取响应对象 */
+        one2one: Boolean = true, /* 请求对响应是一对一(ResponseType是非List), 还是一对多(ResponseType是List) */
+        flushSize: Int = 100 /* 触发刷盘的队列大小 */,
+        flushTimeoutMillis: Long = 100 /* 触发刷盘的定时时间 */,
+        batchFutureSupplier:(List<RequestArgumentType>) -> CompletableFuture<List<BatchItemType>> /* 批量取值操作 */
+): GroupFutureSupplierCombiner<RequestArgumentType, ResponseType, BatchItemType>(reqArgField, respField, one2one, flushSize, flushTimeoutMillis, batchFutureSupplier){
+
+    public constructor(batchMethod: Method, reqArgField: String, respField: String = "", one2one: Boolean = true, flushSize: Int = 100, flushTimeoutMillis: Long = 100)
+            :this(reqArgField, respField, one2one, flushSize, flushTimeoutMillis, dispatcher.method2DispatchLambda(batchMethod))
+
+    public constructor(batchFunc: KFunction2<*, List<RequestArgumentType>, List<ResponseType>>, reqArgField: String, respField: String = "", one2one: Boolean = true, flushSize: Int = 100, flushTimeoutMillis: Long = 100)
+            :this(reqArgField, respField, one2one, flushSize, flushTimeoutMillis, dispatcher.method2DispatchLambda(batchFunc.javaMethod!!))
+
+    public constructor(batchMethod: Method, annotation: GroupCombine):this(batchMethod, annotation.reqArgField, annotation.respField, annotation.one2one, annotation.flushSize, annotation.flushTimeoutMillis)
+
+    companion object {
+        /**
+         * 请求分发者
+         */
+        protected val dispatcher: IRpcRequestDispatcher = RcpRequestDispatcher
+
+        /**
+         * 缓存方法的rpc请求合并器: <方法 to GroupRpcRequestCombiner>
+         */
+        protected val groupCombiners: ConcurrentHashMap<Method, GroupRpcRequestCombiner<Any, Any?, Any>> = ConcurrentHashMap();
+
+        /**
+         * 获得方法对应的MethodHandle对象
+         * @param method
+         * @return
+         */
+        public fun instance(method: Method): GroupRpcRequestCombiner<Any, Any?, Any> {
+            return groupCombiners.getOrPut(method){
+                val annotation = method.groupCombine!!
+                val batchMethod = method.declaringClass.methods.first { it.name == annotation.batchMethod }
+                GroupRpcRequestCombiner(batchMethod, annotation)
+            }
+        }
+    }
+
+
+}
--- jksoa-client/src/test/kotlin/net/jkcode/jksoa/tests/RpcRequestCombinerTests.kt
+++ jksoa-client/src/test/kotlin/net/jkcode/jksoa/tests/RpcRequestCombinerTests.kt
@@ -1,2 +1,118 @@
 package net.jkcode.jksoa.tests
 
+import net.jkcode.jkmvc.common.print
+import net.jkcode.jkmvc.common.randomString
+import net.jkcode.jkmvc.common.startAndJoin
+import net.jkcode.jksoa.client.combiner.GroupRpcRequestCombiner
+import net.jkcode.jksoa.client.combiner.KeyRpcRequestCombiner
+import net.jkcode.jksoa.client.referer.Referer
+import net.jkcode.jksoa.example.IExampleService
+import net.jkcode.jksoa.example.User
+import org.junit.Test
+import java.util.concurrent.CompletableFuture
+
+/**
+ * @Description:
+ * @author shijianhang<772910474@qq.com>
+ * @date 2017-12-14 3:11 PM
+ */
+class RpcRequestCombinerTests {
+
+    val combinerService = Referer.getRefer<IExampleService>()
+
+    /************************* 测试 KeyRpcRequestCombiner **************************/
+    /**
+     * 测试 KeyRpcRequestCombiner -- 手动调用 -- 能合并
+     */
+    @Test
+    fun testKeyRpcRequestCombiner() {
+        val keyCombiner = KeyRpcRequestCombiner(IExampleService::getUserById)
+        val futures = ArrayList<CompletableFuture<User>>()
+        for (i in (0..2)) {
+            futures.add(keyCombiner.add(1))
+        }
+        futures.print()
+    }
+
+    /**
+     * 测试 GroupRpcRequestCombiner -- 通过调用目标方法来调用 -- 同步调用不能合并
+     */
+    @Test
+    fun testKeyRpcRequestCombiner2() {
+        // 同步调用
+        val run = {
+            val id = 1
+            val u = combinerService.getUserById(id)
+            println("调用服务[IExampleService.getUserById($id)]结果： $u")
+        }
+        for (i in (0..2)) {
+            Thread(run).startAndJoin()
+        }
+    }
+
+
+    /**
+     * 测试 testKeyRpcRequestCombiner -- 通过调用目标方法来调用 -- 异步调用能合并
+     */
+    @Test
+    fun testKeyRpcRequestCombiner3() {
+        // 异步调用
+        (0..2).forEach {
+            val id = 1
+            val f = combinerService.getUserByIdAsync(id)
+            f.thenAccept{
+                println("调用服务[IExampleService.getUserById($id)]结果： $it")
+            }
+        }
+        Thread.sleep(2000)
+    }
+
+    /************************* 测试 GroupRpcRequestCombiner **************************/
+    /**
+     * 测试 GroupRpcRequestCombiner -- 手动调用 -- 能合并
+     */
+    @Test
+    fun testGroupRpcRequestCombiner() {
+        val groupCombiner = GroupRpcRequestCombiner<String, User, User>(IExampleService::listUsersByName, "name", "", true, 100, 100)
+        val futures = ArrayList<CompletableFuture<User>>()
+        for (i in (0..2)) {
+            futures.add(groupCombiner.add(randomString(7))!!)
+        }
+        futures.print()
+    }
+
+    /**
+     * 测试 GroupRpcRequestCombiner -- 通过调用目标方法来调用 -- 同步调用不能合并
+     */
+    @Test
+    fun testGroupRpcRequestCombiner2() {
+        // 同步调用
+        val run = {
+            val name = randomString(7)
+            val u = combinerService.getUserByName(name)
+            println("调用服务[IExampleService.getUserByName($name)]结果： $u")
+        }
+        for (i in (0..2)) {
+            Thread(run).startAndJoin()
+        }
+    }
+
+    /**
+     * 测试 GroupRpcRequestCombiner -- 通过调用目标方法来调用 -- 异步调用能合并
+     */
+    @Test
+    fun testGroupRpcRequestCombiner3() {
+        // 异步调用
+        for (i in (0..2)) {
+            val name = randomString(7)
+            val f = combinerService.getUserByNameAsync(name)
+            f.thenAccept {
+                println("调用服务[IExampleService.getUserByName($name)]结果： $it")
+            }
+        }
+        Thread.sleep(2000)
+    }
+
+
+
+}
