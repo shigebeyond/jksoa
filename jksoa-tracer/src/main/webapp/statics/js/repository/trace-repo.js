473e77ff4a94695d2b16e3ad58f59f315bedec78
shijianhang <772910474.com>
2019-07-04 19:49:41 +0800

    添加tracer 前端代码

---
+++ jksoa-tracer/src/main/webapp/statics/js/repository/trace-repo.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright jd
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+'use strict';
+angular.module('hydra.repository.trace', ['ngResource'])
+    //查询跟踪详细信息
+    .factory('Trace', function ($resource) {
+        return $resource(ctp + '/trace/info/:traceId');
+    })
+    // 根据耗时来查询跟踪信息
+    .factory('TraceList', function ($resource) {
+        // 参数: serviceId/startTime/durationMin/durationMax/sum
+        return $resource(ctp + '/trace/list', {}, {
+            getTraceList:{
+                method:'GET',
+                isArray:true
+            }
+        });
+    })
+    // 根据异常来查询跟踪信息
+    .factory('TraceListEx', function ($resource) {
+        return $resource(ctp + '/trace/listEx/:serviceId/:startTime/:sum', {}, {
+            getTraceList:{
+                method:'GET',
+                isArray:true
+            }
+        });
+    });
