e130fbc2283b8bd438ae8bc3e7a5dfc9381afcfd
shijianhang <772910474.com>
2019-07-17 12:33:15 +0800

    完善异常

---
+++ jksoa-common/src/main/kotlin/net/jkcode/jksoa/common/exception/RpcNoConnectionException.kt
@@ -0,0 +1,18 @@
+package net.jkcode.jksoa.common.exception
+
+import net.jkcode.jkmvc.common.JkException
+
+/**
+ * rpc时没有连接的异常
+ *    在 IConnectionHub.select()/selectAll()中找不到连接时抛出该异常
+ */
+class RpcNoConnectionException : JkException {
+    public constructor(message: String) : super(message) {
+    }
+
+    public constructor(cause: Throwable) : super(cause) {
+    }
+
+    public constructor(message: String, cause: Throwable) : super(message, cause) {
+    }
+}
