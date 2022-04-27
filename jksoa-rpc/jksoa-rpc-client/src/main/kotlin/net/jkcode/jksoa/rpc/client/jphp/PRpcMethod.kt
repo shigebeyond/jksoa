74fec890d15b20b749118dc4b121f90d10a05b1b
shijianhang <3076679995@qq.com>
2022-04-27 18:49:26 +0800

    优化代码

--- jksoa-rpc/jksoa-rpc-client/src/main/kotlin/net/jkcode/jksoa/rpc/client/jphp/PRpcMethod.kt
+++ jksoa-rpc/jksoa-rpc-client/src/main/kotlin/net/jkcode/jksoa/rpc/client/jphp/PRpcMethod.kt
@@ -27,9 +27,9 @@ import java.lang.reflect.InvocationTargetException
  *    php中的实例化: $ref = new Referer("net.jkcode.jksoa.rpc.example.ISimpleService");
  *    php中的方法调用（默认方法）: $ref->ping();
  */
-@Reflection.Name("Referer")
+@Reflection.Name("RpcMethod")
 @Reflection.Namespace(JksoaRpcExtension.NS)
-open class PDynReferer(env: Environment, clazz: ClassEntity) : BaseObject(env) {
+open class PRpcMethod(env: Environment, clazz: ClassEntity) : BaseObject(env) {
 
     protected lateinit var clazz: String // 远程类
     protected lateinit var methodSignature: String // 远程方法
@@ -116,117 +116,19 @@ open class PDynReferer(env: Environment, clazz: ClassEntity) : BaseObject(env) {
 
     @Reflection.Signature
     fun getClassName(env: Environment, vararg args: Memory): Memory {
-        return StringMemory(referer.serviceId)
+        return StringMemory(clazz)
     }
 
     companion object {
 
         /**
-         * 创建 PReferer 实例
+         * 创建 PRpcMethod 实例
          */
-        fun of(env: Environment, clazzName: String): PReferer {
-            val javaObject = PReferer(env, env.fetchClass(JksoaRpcExtension.NS + "\\Referer"))
-            javaObject.referer = getRef(clazzName)
+        fun of(env: Environment, clazzName: String, methodSignature: String): PRpcMethod {
+            val javaObject = PRpcMethod(env, env.fetchClass(JksoaRpcExtension.NS + "\\RpcMethod"))
+            javaObject.__construct(clazzName, methodSignature)
             return javaObject
         }
 
-        /**
-         * 获得被包装的服务引用对象
-         */
-        private fun getRef(clazzName: String): Referer {
-            val referer = RefererLoader.get(clazzName) as Referer?
-            if (referer == null)
-                throw RpcClientException("未加载远程服务: " + clazzName)
-            return referer
-        }
-    }
-}
-
-/**
- * 包装服务的引用对象
- * 1 调用服务的引用对象（代理）
- *    仿jphp自带的 JavaObject，但该类并不能动态调用方法
- *    动态调用方法的实现，使用魔术方法
- *    注意：不能直接使用`dispatcher.dispatch(req)`来发rpc请求，必须通过调用引用（代理）对象来发，以便`JavaMethod.invokeArgs()`根据代理的java方法将php参数转换为正确的java参数类型
- * 2 使用
- *    java中的实例化： val ref = PReferer.of(env, "net.jkcode.jksoa.rpc.example.ISimpleService")
- *    php中的实例化: $ref = new Referer("net.jkcode.jksoa.rpc.example.ISimpleService");
- *    php中的方法调用（默认方法）: $ref->ping();
- */
-@Reflection.Name("Referer")
-@Reflection.Namespace(JksoaRpcExtension.NS)
-open class `PDynReferer.kt`(env: Environment, clazz: ClassEntity) : BaseObject(env) {
-
-    // 被包装的服务的引用对象
-    lateinit var referer: Referer
-
-    @Reflection.Signature
-    fun __construct(clazzName: String): Memory {
-        this.referer = getRef(clazzName)
-        return Memory.NULL
-    }
-
-    //__call()实现一： wrong, 不严谨，因为jphp自动转换的实参类型（如数字会转为Long），可能对不上方法的形参类型(如int)
-    /*@Reflection.Signature
-    fun __call(name: String, vararg args: Any?): Any? {
-        try {
-            // 获得方法
-            val method = obj.javaClass.getMethodByName(name)
-            if(method == null)
-                throw NoSuchMethodException("类[${obj.javaClass}]无方法[$name]")
-            // 调用方法
-            return method.invoke(obj, *args)
-        } catch (e: Exception) {
-            JavaReflection.exception(env, e)
-        }
-        return Memory.NULL
-    }*/
-
-    //__call()实现二： 使用 JavaMethod 包装方法调用
-    @Reflection.Signature(value = [Reflection.Arg("name"), Reflection.Arg("arguments")])
-    fun __call(env: Environment, vararg args: Memory): Memory {
-        try {
-            // 第一个参数是方法名
-            val name = args[0].toString()
-            // 获得方法
-            val method = referer.`interface`.getMethodByName(name)
-            if(method == null)
-                throw NoSuchMethodException("类[${referer.`interface`}]无方法[$name]")
-            // 用 JavaMethod 包装方法调用
-            val method2 = JavaMethod.of(env, method)
-            val args2 = args.toMutableList()
-            args2[0] = ObjectMemory(JavaObject.of(env, referer.service)) // 第一个参数，原来是方法名，现替换为被包装的服务的引用对象
-            return method2.invokeArgs(env, *args2.toTypedArray())
-        } catch (e: Exception) {
-            JavaReflection.exception(env, e)
-        }
-        return Memory.NULL
-    }
-
-    @Reflection.Signature
-    fun getClassName(env: Environment, vararg args: Memory): Memory {
-        return StringMemory(referer.serviceId)
-    }
-
-    companion object {
-
-        /**
-         * 创建 PReferer 实例
-         */
-        fun of(env: Environment, clazzName: String): `PDynReferer.kt` {
-            val javaObject = `PDynReferer.kt`(env, env.fetchClass(JksoaRpcExtension.NS + "\\Referer"))
-            javaObject.referer = getRef(clazzName)
-            return javaObject
-        }
-
-        /**
-         * 获得被包装的服务引用对象
-         */
-        private fun getRef(clazzName: String): Referer {
-            val referer = RefererLoader.get(clazzName) as Referer?
-            if (referer == null)
-                throw RpcClientException("未加载远程服务: " + clazzName)
-            return referer
-        }
     }
 }
